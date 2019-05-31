96677710f52346fd423eb22380895f96eeb688f6
Jian Cao <sworden.cao@gmail.com>
2019-05-31 22:18:14 +0800

    modulespath: add mpi and hpl
    Signed-off-by: Jian Cao <sworden.cao@gmail.com>
    Package-Manager: Portage-2.3.66, Repoman-2.3.11

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 cd $(dirname "$0")
 #emerge -a sys-cluster/modules
-cp -f modulefile/mpi/* /etc/modulefiles
-cp -f modulefile/hpl /etc/modulefiles
+cp -f modulefile/mpi/* /etc/modulefiles/mpi
+cp -f modulefile/hpl /etc/modulefiles/hpl
+cp -f modulespath /usr/share/Modules/init/.modulespath
