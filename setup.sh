19560dc4e2718504165ccebe5515db19ec6eef67
Jian Cao <sworden.cao@gmail.com>
2019-06-03 23:12:20 +0800

    eselect: add mpi and hpl skeleton files
    Signed-off-by: Jian Cao <sworden.cao@gmail.com>
    Package-Manager: Portage-2.3.66, Repoman-2.3.11

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@
 
 cd $(dirname "$0")
 #emerge -a sys-cluster/modules
+
+# update modulefiles
 cp -f modulefile/mpi/* /etc/modulefiles/mpi
 cp -f modulefile/hpl /etc/modulefiles/hpl
 cp -f modulespath /usr/share/Modules/init/.modulespath
+
+# update eselect files
+# TODO: probably hpl (or similar) packages here should consider name conflict
+cp -f mpi.eselect /usr/share/eselect/modules/mpi.eselect
+cp -f hpl.eselect /usr/share/eselect/modules/hpl.eselect
